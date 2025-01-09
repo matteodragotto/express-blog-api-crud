70968965cc0191e45b3bc4b304d3b3c051e30add
matteodragotto <matteodrag8@gmail.com>
2025-01-09 12:06:20 +0100

    [feat] not found middleware

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const postsRouter = require('./router/posts')
-
 const app = express()
 const port = 3001
+
+const notFound = require('./middlewares/notFound')
+const errorsHandler = require('./middlewares/errorsHandler')
 
 app.use(express.json())
 
@@ -11,6 +13,10 @@ app.get('/', (req, res) => {
 })
 
 app.use('/posts', postsRouter);
+
+app.use(errorsHandler)
+app.use(notFound);
+
 
 app.listen(port, () => {
   console.log('Sono in ascolto sulla porta 3001')
